9350a8544abae25a70aa00d2fd355031cec97aa6
humdan123 <humza.afzal77@gmail.com>
2020-02-10 14:55:19 -0500

    Created projectile.py file and added constructor

---
+++ projectile.py
@@ -0,0 +1,9 @@
+class Projectile:
+
+    def __init__(self, type, damage):
+        """
+        type: what type of projectile it is, string
+        damage: how much damage it does, int
+        """
+        self.type = type
+        self.damage = damage
