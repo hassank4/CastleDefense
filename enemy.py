f7d67447ef62ecf0d86b4c46e426b91c55aefe78
kevinsub <kevinsubhash@gmail.com>
2020-02-09 19:32:10 -0500

    [CTRLAL-3] Created enemy class

---
+++ enemy.py
@@ -0,0 +1,25 @@
+"""
+This class is for the Enemy object. This class allows us to keep track of
+an Enemy's id as well as it's health. An Enemy can lose its health.
+The health of an Enemy can be as high as 1000 or as low as 0.
+"""
+
+class Enemy:
+    def __init__(self, id): # Constructor to create an Enemy object
+        self.id = id
+        self.health = 100
+        self.image = ""
+
+    def __str__(self): # Function to output Enemy health
+        return "Enemy " + str(self.id) + ": " + str(self.health) + " Health"
+
+    def getHealth(self): # Function to get the Enemy health
+        return self.health
+
+    def getId(self): # Function to get the Enemy id
+        return self.id
+
+    def subHealth(self, amount): # Function to deduct Enemy health
+        self.health = self.health - amount
+        if(self.health < 0):
+            self.health = 0
