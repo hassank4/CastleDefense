b73f1b21bba3082b64c9c91511b07109a9de3a64
humdan123 <humza.afzal77@gmail.com>
2020-02-12 11:40:46 -0500

    updated projetile.py

--- projectile.py
+++ projectile.py
@@ -1,9 +1,24 @@
 class Projectile:
 
-    def __init__(self, type, damage):
+    def __init__(self, type, damage, hit=False):
         """
         type: what type of projectile it is, string
         damage: how much damage it does, int
         """
         self.type = type
         self.damage = damage
+        self.hit = hit
+        self.exist = True
+
+    def destroy(self):
+        if (self.hit):
+            self.exist = False
+
+    def is_hit(self, enemy=None):
+        if not (type(enemy) == None):
+            self.hit = True
+            self.deal_damage(enemy)
+            self.destroy()
+    
+    def deal_damage(self, enemy):
+        enemy.deal_damage(self.damage)
