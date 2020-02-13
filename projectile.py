265cadad69fad024bb256a71f466a1674cef037e
humdan123 <humza.afzal77@gmail.com>
2020-02-12 20:41:07 -0500

    Updated projectile.py class

--- projectile.py
+++ projectile.py
@@ -1,7 +1,11 @@
 class Projectile:
-
+    """
+    Class of the projectile that is shooted by the Defense Object onto an Enemy Object
+    """
+    
     def __init__(self, type, damage, hit=False):
         """
+        Creates an instance of the Projectile Class
         type: what type of projectile it is, string
         damage: how much damage it does, int
         """
@@ -11,14 +15,23 @@ class Projectile:
         self.exist = True
 
     def destroy(self):
+        """
+        Removes the Projetile from the game
+        """
         if (self.hit):
             self.exist = False
 
     def is_hit(self, enemy=None):
+        """
+        Decides if the enemy has been hit or not and calls the relevant methods
+        """
         if not (type(enemy) == None):
             self.hit = True
             self.deal_damage(enemy)
             self.destroy()
     
     def deal_damage(self, enemy):
-        enemy.deal_damage(self.damage)
+        """
+        Deals damage to the enemy
+        """
+        enemy.sub_health(self.damage)
