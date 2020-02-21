f8e82d69d28b23f275d2d05f6215b7d361fe672a
Bikramjit Saini <bikramjitsaini25@gmail.com>
2020-02-21 12:03:27 -0500

    [CTRLAL-16] Added methods in Enemy for getting current position and moving to next position

--- src/Enemy.py
+++ src/Enemy.py
@@ -9,10 +9,12 @@ class Enemy:
         self.id = id
         self.health = 100
         self.image = image
-        # The initial point
-        self.initial = (497, 2070)
-        # The list of pixels where the Enemy will have to go to
-        self.points = [(395, 905), (255, 873), (179, 807), (156, 686), (236, 576), (341, 543), (450, 497), (495, 393), (516, 293), (635, 209), (951, 206), (1091, 332), (1092, 657), (1181, 786), (1899, 786)]
+        # The list of pixels where the Enemy will have to go to (1920x1080)
+        self.points = [(497, 1070), (395, 905), (255, 873), (179, 807), (156, 686), (236, 576), (341, 543), (450, 497), (495, 393), (516, 293), (635, 209), (951, 206), (1091, 332), (1092, 657), (1181, 786), (1899, 786)]
+        # The list of pixels where the Enemy will have to go to (1000x600)
+        # self.points = [(497, 1070), (395, 905), (255, 873), (179, 807), (156, 686), (236, 576), (341, 543), (450, 497), (495, 393), (516, 293), (635, 209), (951, 206), (1091, 332), (1092, 657), (1181, 786), (1899, 786)]
+        # The index of the Enemy's current point from the list of points
+        self.current = 0
 
     def __str__(self): # Function to output Enemy health
         return "Enemy " + str(self.id) + ": " + str(self.health) + " Health"
@@ -28,5 +30,13 @@ class Enemy:
         if(self.health < 0):
             self.health = 0
 
-    def movePath(self): # Function to move through points in self.points
-        pass
+    def getPosition(self):  # Function to get the Enemy position
+        return self.points[self.current]
+
+    def moveToNextPoint(self): # Function to move through points in self.points and return whether move was successful or not
+        # Checking if the Enemy is already at the last point
+        if (self.current == (len(self.points) - 1)):
+            return False
+        else:
+            self.current += 1
+            return True
