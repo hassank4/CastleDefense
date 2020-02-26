e4b64ec5129132522cf7e45a710eb75d788696e6
kamalha4 <h.kamal@mail.utoronto.ca>
2020-02-25 19:14:21 -0500

    [CTRLAL-13] Updated the defense and projectile classes

--- src/Defense.py
+++ src/Defense.py
@@ -3,39 +3,23 @@ class Defense:
     This is the Defense Class that contains the essentials of a defense character in the game.
     """
 
-    def __init__(self, id, health, attack_damage, level, x_coord, y_coord, image):
+    def __init__(self, id, attack_damage, level, x_coord, y_coord, idle_image, attack_image):
         """
         This initializes a defense object.
         """
         self.id = id
-        self.health = health
         self.attack_damage = attack_damage
         self.level = level
         self.x = x_coord
         self.y = y_coord
-        self.image = image
+        self.idle_image = idle_image
+        self.attack_image = attack_image
 
     def get_id(self):
         """
         Return the id associated with the defense character.
         """
         return self.id
-
-    def get_health(self) -> int:
-        """
-        Return the health of the defense character.
-        """
-        return self.health
-
-    def update_health(self, attack:int):
-        """
-        Updates the health of the defense character based on how much it was attacked.
-        :param attack: The attack damage done by the enemy
-        """
-        if (self.health - attack) < 0:
-            self.health = 0
-        else:
-            self.health = self.health - attack
 
     def get_attack_damage(self):
         """
@@ -54,12 +38,9 @@ class Defense:
         """
         Return whether the defense character is at max level or not.
         """
-        if self.level == 3:
-            return True
-        else:
-            return False
+        return self.level == 3
 
-    def set_level(self, level: int) -> int:
+    def set_level(self, level: int):
         """
         Sets the level of the defense character
         :param level: The level of the character
@@ -79,15 +60,14 @@ class Defense:
         self.x = x_coord
         self.y = y_coord
 
-    def if_dead(self) -> bool:
+    def get_idle_image(self):
         """
-        Returns whether the defense character is dead and can no longer fight the enemies.
+        Get the image associated with the character when idled
         """
-        if self.health == 0:
-            return True
-        else:
-            return False
+        return self.idle_image
 
-    
-
-
+    def get_attack_image(self):
+        """
+        Get the image associated with the character when attacking
+        """
+        return self.attack_image
