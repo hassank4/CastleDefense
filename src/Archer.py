6a1011f4927b817a3b5041e0565f5219a0364838
kamalha4 <hassan.k4@hotmail.com>
2020-02-26 15:04:59 -0500

    [CTRLAL-13] Created classes for the 3 defense characters

---
+++ src/Archer.py
@@ -0,0 +1,36 @@
+from src.Defense import Defense
+from src.Projectile import Projectile
+
+
+class WeakArcher(Defense):
+
+    def __init__(self, archer_id, x_coord, y_coord):
+        super().__init__(archer_id, 15, 1, x_coord, y_coord, "Images/Defense/archer1/idle1.png"
+                         , "Images/Defense/archer1/attack2.png")
+        self.projectile = Projectile("arrow", super().get_attack_damage(), "Images/Projectile/archer/basic_arrow1.png")
+
+    def get_projectile(self):
+        return self.projectile
+
+
+class IntermediateArcher(Defense):
+
+    def __init__(self, archer_id, x_coord, y_coord):
+        super().__init__(archer_id, 20, 2, x_coord, y_coord, "Images/Defense/archer2/idle1.png"
+                         , "Images/Defense/archer2/attack2.png")
+        self.projectile = Projectile("arrow", super().get_attack_damage(),
+                                     "Images/Projectile/archer/blue_fire_arrow1.png")
+
+    def get_projectile(self):
+        return self.projectile
+
+
+class StrongArcher(Defense):
+
+    def __init__(self, archer_id, x_coord, y_coord):
+        super().__init__(archer_id, 25, 3, x_coord, y_coord, "Images/Defense/archer3/idle1.png"
+                         , "Images/Defense/archer3/attack2.png")
+        self.projectile = Projectile("arrow", super().get_attack_damage(), "Images/Projectile/archer/fire_arrow1.png")
+
+    def get_projectile(self):
+        return self.projectile
