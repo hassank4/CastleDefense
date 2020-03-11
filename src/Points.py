18e654c33e61af26ed1fa5245db5b607d290f15a
kamalha4 <hassan.k4@hotmail.com>
2020-03-11 16:14:19 -0400

    [CTRLAL-21] Added more methods to the Points class

--- src/Points.py
+++ src/Points.py
@@ -13,6 +13,9 @@ class Points:
         Initializes the point object.
         """
         self.score = 0
+        self.assassin_kills = 0
+        self.mage_kills = 0
+        self.ogre_kills = 0
 
     def get_points(self):
         """
@@ -22,19 +25,32 @@ class Points:
 
     def update_score(self, enemy):
         """
-        Changes the number of points depending on which enemy is killed.
+        Changes the number of points and kills depending on which enemy is killed.
         """
-        if enemy.get_health() == 0:
+        if enemy.getHealth() == 0:
             if type(enemy) == Assassin:
                 self.score += 5
+                self.assassin_kills += 1
                 print(type(enemy))
             elif type(enemy) == Mage:
                 self.score += 10
+                self.mage_kills += 1
             elif type(enemy) == Ogre:
                 self.score += 20
+                self.ogre_kills += 1
 
     def reset(self):
         """
         Sets the score back to 0 for when the users decides to play the game again.
         """
         self.score = 0
+        self.assassin_kills = 0
+        self.mage_kills = 0
+        self.ogre_kills = 0
+
+    def get_kills(self):
+        """
+        Returns the amount of kills for the three types of enemies.
+        """
+        return [self.assassin_kills, self.mage_kills, self.ogre_kills]
+
